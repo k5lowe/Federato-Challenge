82f8a97b3dbcc76bb79cf920598f9017c0673d2c
Kushini <kushinirita@gmail.com>
2025-02-21 16:07:07 -0500

    Test

---
+++ Federato_Challenge.py
@@ -0,0 +1,68 @@
+
+import ijson
+import pandas as pd
+from pathlib import Path
+
+#file year 
+year = 2025
+file_path = r"amplitude_export_2025.json\new_amplitude_export_2025.json"
+
+
+#non-empty columns
+columns_keep = [
+    "$insert_id",
+    "amplitude_id",
+    "app",
+    "city",
+    "client_event_time",
+    "client_upload_time",
+    "country",
+    "data",
+    "data_type",
+    "device_family",
+    "device_id",
+    "device_type",
+    "dma",
+    "event_id",
+    "event_properties",
+    "event_time",
+    "event_type",
+    "language",
+    "library",
+    "os_name",
+    "os_version",
+    "platform",
+    "processed_time",
+    "region",
+    "server_received_time",
+    "server_upload_time",
+    "session_id",
+    "user_id",
+    "user_properties",
+    "uuid",
+]
+path = Path(f"{year}_csv")
+if not path.exists():
+    path.mkdir(parents=True, exist_ok=True)
+#use ijson to read the json files efficiently in memory
+with open(file_path, "r") as f:
+    objects = ijson.items(f, "item") #creates a generator object
+    
+    batch_size = 100000 #can be updated, currently saves per batches of 100,000
+    chunk = []
+    count = 0 #used to index batch file
+    for obj in objects:
+        chunk.append(obj)
+        if len(chunk) >= batch_size:
+            df = pd.DataFrame(chunk)
+            output_csv = f"{year}_csv/{file_path.split('.')[0]}_chunk_{count*batch_size}_{(count+1)*batch_size}.csv"
+            df = df[columns_keep] #remove empty columns
+            df.to_csv(output_csv, index=False)
+            count += 1
+            chunk = []
+
+    if chunk: #process remaining data if any
+        output_csv = f"{year}_csv/{file_path.split('.')[0]}_chunk_{count*batch_size}_{(count+1)*batch_size}.csv"
+        df = pd.DataFrame(chunk)
+        df = df[columns_keep]
+        df.to_csv(output_csv, index=False)
